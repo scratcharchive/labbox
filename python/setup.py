3cf62d1f986ff1e595672f2cbdeaa659c0f461a0
Jeremy Magland <jeremy.magland@gmail.com>
2021-03-16 17:01:38 -0400

    incr version to use kachery-p2p 0.8

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='labbox',
-    version='0.1.25',
+    version='0.1.26',
     author="Jeremy Magland",
     author_email="jmagland@flatironinstitute.org",
     description="",
@@ -19,7 +19,7 @@ setuptools.setup(
     install_requires=[
         'numpy',
         'hither>=0.4.2',
-        'kachery-p2p>=0.6.2',
+        'kachery-p2p>=0.8.0',
         'websockets',
         'pyyaml',
         'aiohttp',
