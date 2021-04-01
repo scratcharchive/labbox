68f357ed6517960b40b9727bf32647ee735dec54
Jeremy Magland <jeremy.magland@gmail.com>
2021-04-01 07:55:02 -0400

    update versions

--- python/setup.py
+++ python/setup.py
@@ -12,8 +12,8 @@ setuptools.setup(
     ],
     install_requires=[
         'numpy',
-        'hither>=0.5.2',
-        'kachery-p2p>=0.8.2',
+        'hither>=0.5.11',
+        'kachery-p2p>=0.8.10',
         'websockets',
         'pyyaml',
         'aiohttp',
